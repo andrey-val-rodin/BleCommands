da4244f454284e9468059e81578a028d3d4234d3
Andrey Rodin <andrey.val.rodin@gmail.com>
2026-04-11 10:55:08 +0300

    Add ListeningStream

---
+++ Source/Core/Contracts/Constants.cs
@@ -0,0 +1,7 @@
+﻿namespace Core.Contracts
+{
+    public class Constants
+    {
+        public const char Terminator = '\n';
+    }
+}
---
+++ Source/Core/Contracts/DeviceInputEventArgs.cs
@@ -0,0 +1,12 @@
+﻿namespace Core.Contracts
+{
+    public class DeviceInputEventArgs : EventArgs
+    {
+        public DeviceInputEventArgs(string text)
+        {
+            Text = text;
+        }
+
+        public string Text { get; private set; }
+    }
+}
